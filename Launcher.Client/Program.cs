3ecaee6822bcd660872dc1319765717c3322a40e
DocNITE <docnite0530@gmail.com>
2023-06-07 13:00:43 +0300

    examples

--- Launcher.Client/Program.cs
+++ Launcher.Client/Program.cs
@@ -1,5 +1,144 @@
-﻿using System;
+﻿/*
 using System.Net.Sockets;
+ 
+string host = "127.0.0.1";
+int port = 8888;
+using TcpClient client = new TcpClient();
+Console.Write("Введите свое имя: ");
+string? userName = Console.ReadLine();
+Console.WriteLine($"Добро пожаловать, {userName}");
+StreamReader? Reader = null;
+StreamWriter? Writer = null;
+ 
+try
+{
+    client.Connect(host, port); //подключение клиента
+    Reader = new StreamReader(client.GetStream());
+    Writer = new StreamWriter(client.GetStream());
+    if (Writer is null || Reader is null) return;
+    // запускаем новый поток для получения данных
+    Task.Run(()=>ReceiveMessageAsync(Reader));
+    // запускаем ввод сообщений
+    await SendMessageAsync(Writer);
+}
+catch (Exception ex)
+{
+    Console.WriteLine(ex.Message);
+}
+Writer?.Close();
+Reader?.Close();
+ 
+// отправка сообщений
+async Task SendMessageAsync(StreamWriter writer)
+{
+    // сначала отправляем имя
+    await writer.WriteLineAsync(userName);
+    await writer.FlushAsync();
+    Console.WriteLine("Для отправки сообщений введите сообщение и нажмите Enter");
+ 
+    while (true)
+    {
+        string? message = Console.ReadLine();
+        await writer.WriteLineAsync(message);
+        await writer.FlushAsync();
+    }
+}
+// получение сообщений
+async Task ReceiveMessageAsync(StreamReader reader)
+{
+    while (true)
+    {
+        try
+        {
+            // считываем ответ в виде строки
+            string? message = await reader.ReadLineAsync();
+            // если пустой ответ, ничего не выводим на консоль
+            if (string.IsNullOrEmpty(message)) continue; 
+            Print(message);//вывод сообщения
+        }
+        catch
+        {
+            break;
+        }
+    }
+}
+// чтобы полученное сообщение не накладывалось на ввод нового сообщения
+void Print(string message)
+{
+    if (OperatingSystem.IsWindows())    // если ОС Windows
+    {
+        var position = Console.GetCursorPosition(); // получаем текущую позицию курсора
+        int left = position.Left;   // смещение в символах относительно левого края
+        int top = position.Top;     // смещение в строках относительно верха
+        // копируем ранее введенные символы в строке на следующую строку
+        Console.MoveBufferArea(0, top, left, 1, 0, top + 1);
+        // устанавливаем курсор в начало текущей строки
+        Console.SetCursorPosition(0, top);
+        // в текущей строке выводит полученное сообщение
+        Console.WriteLine(message);
+        // переносим курсор на следующую строку
+        // и пользователь продолжает ввод уже на следующей строке
+        Console.SetCursorPosition(left, top + 1);
+    }
+    else Console.WriteLine(message);
+}
+TCP EXAMPLE */
 
-// See https://aka.ms/new-console-template for more information
-var socket = new TcpClient();
+/*
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+ 
+IPAddress localAddress = IPAddress.Parse("127.0.0.1");
+Console.Write("Введите свое имя: ");
+string? username = Console.ReadLine();
+Console.Write("Введите порт для приема сообщений: ");
+if (!int.TryParse(Console.ReadLine(), out var localPort)) return;
+Console.Write("Введите порт для отправки сообщений: ");
+if (!int.TryParse(Console.ReadLine(), out var remotePort)) return;
+Console.WriteLine();
+ 
+// запускаем получение сообщений
+Task.Run(ReceiveMessageAsync);
+// запускаем ввод и отправку сообщений
+await SendMessageAsync();
+ 
+// отправка сообщений в группу
+async Task SendMessageAsync()
+{
+    using Socket sender = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+    Console.WriteLine("Для отправки сообщений введите сообщение и нажмите Enter");
+    // отправляем сообщения
+    while (true)
+    {
+        var message = Console.ReadLine(); // сообщение для отправки
+        // если введена пустая строка, выходим из цикла и завершаем ввод сообщений
+        if (string.IsNullOrWhiteSpace(message)) break;
+        // иначе добавляем к сообщению имя пользователя
+        message = $"{username}: {message}";
+        byte[] data = Encoding.UTF8.GetBytes(message);
+        // и отправляем на 127.0.0.1:remotePort
+        await sender.SendToAsync(data, new IPEndPoint(localAddress, remotePort));
+    }
+}
+// отправка сообщений
+async Task ReceiveMessageAsync()
+{
+    byte[] data = new byte[65535]; // буфер для получаемых данных
+    // сокет для прослушки сообщений
+    using Socket receiver = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+    // запускаем получение сообщений по адресу 127.0.0.1:localPort
+    receiver.Bind(new IPEndPoint(localAddress, localPort));
+    while (true)
+    {
+        // получаем данные в массив data
+        var result = await receiver.ReceiveFromAsync(data, new IPEndPoint(IPAddress.Any, 0));
+        var message = Encoding.UTF8.GetString(data, 0, result.ReceivedBytes);
+        // выводим сообщение
+        Console.WriteLine(message);
+    }
+}
+*/
+
+
+// TODO - Make with httpClient
--- Launcher.Server/Program.cs
+++ Launcher.Server/Program.cs
@@ -1,3 +1,138 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-Console.ReadLine();
+﻿/*
+using System.Net;
+using System.Net.Sockets;
+ 
+ServerObject server = new ServerObject();// создаем сервер
+await server.ListenAsync(); // запускаем сервер
+ 
+class ServerObject
+{
+    TcpListener tcpListener = new TcpListener(IPAddress.Any, 8888); // сервер для прослушивания
+    List<ClientObject> clients = new List<ClientObject>(); // все подключения
+    protected internal void RemoveConnection(string id)
+    {
+        // получаем по id закрытое подключение
+        ClientObject? client = clients.FirstOrDefault(c => c.Id == id);
+        // и удаляем его из списка подключений
+        if (client != null) clients.Remove(client);
+        client?.Close();
+    }
+    // прослушивание входящих подключений
+    protected internal async Task ListenAsync()
+    {
+        try
+        {
+            tcpListener.Start();
+            Console.WriteLine("Сервер запущен. Ожидание подключений...");
+ 
+            while (true)
+            {
+                TcpClient tcpClient = await tcpListener.AcceptTcpClientAsync();
+ 
+                ClientObject clientObject = new ClientObject(tcpClient, this);
+                clients.Add(clientObject);
+                Task.Run(clientObject.ProcessAsync);
+            }
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex.Message);
+        }
+        finally
+        {
+            Disconnect();
+        }
+    }
+ 
+    // трансляция сообщения подключенным клиентам
+    protected internal async Task BroadcastMessageAsync(string message, string id)
+    {
+        foreach (var client in  clients)
+        {
+            if (client.Id != id) // если id клиента не равно id отправителя
+            {
+                await client.Writer.WriteLineAsync(message); //передача данных
+                await client.Writer.FlushAsync();
+            }
+        }
+    }
+    // отключение всех клиентов
+    protected internal void Disconnect()
+    {
+        foreach (var client in clients)
+        {
+            client.Close(); //отключение клиента
+        }
+        tcpListener.Stop(); //остановка сервера
+    }
+}
+class ClientObject
+{
+    protected internal string Id { get;} = Guid.NewGuid().ToString();
+    protected internal StreamWriter Writer { get;}
+    protected internal StreamReader Reader { get;}
+ 
+    TcpClient client;
+    ServerObject server; // объект сервера
+ 
+    public ClientObject(TcpClient tcpClient, ServerObject serverObject)
+    {
+        client = tcpClient;
+        server = serverObject;
+        // получаем NetworkStream для взаимодействия с сервером
+        var stream = client.GetStream();
+        // создаем StreamReader для чтения данных
+        Reader = new StreamReader(stream);
+        // создаем StreamWriter для отправки данных
+        Writer = new StreamWriter(stream);
+    }
+ 
+    public async Task ProcessAsync()
+    {
+        try
+        {
+            // получаем имя пользователя
+            string? userName = await Reader.ReadLineAsync();
+            string? message = $"{userName} вошел в чат";
+            // посылаем сообщение о входе в чат всем подключенным пользователям
+            await server.BroadcastMessageAsync(message, Id);
+            Console.WriteLine(message);
+            // в бесконечном цикле получаем сообщения от клиента
+            while (true)
+            {
+                try
+                {
+                    message = await Reader.ReadLineAsync();
+                    if (message == null) continue;
+                    message = $"{userName}: {message}";
+                    Console.WriteLine(message);
+                    await server.BroadcastMessageAsync(message, Id);
+                }
+                catch
+                {
+                    message = $"{userName} покинул чат";
+                    Console.WriteLine(message);
+                    await server.BroadcastMessageAsync(message, Id);
+                    break;
+                }
+            }
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e.Message);
+        }
+        finally
+        {
+            // в случае выхода из цикла закрываем ресурсы
+            server.RemoveConnection(Id);
+        }
+    }
+    // закрытие подключения
+    protected internal void Close()
+    {
+        Writer.Close();
+        Reader.Close();
+        client.Close();
+    }
+}
+Tcp example */
