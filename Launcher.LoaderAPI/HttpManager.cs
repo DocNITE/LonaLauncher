71b2fef8a9fe3a21fc88cc7c088e90433569f215
DocNITE <docnite0530@gmail.com>
2023-07-20 20:47:00 +0300

    Lokl

--- Launcher.LoaderAPI/HttpManager.cs
+++ Launcher.LoaderAPI/HttpManager.cs
@@ -3,6 +3,6 @@ namespace Launcher.LoaderAPI;
 public partial class HttpManager
 {
     private bool _isDownload = false;
-    public string CacheFileName = "Cache";
-    public string CacheDirectory = "./.cache";
+    public const string CacheFileName = "Cache";
+    public const string CacheDirectory = "./.cache";
 }
